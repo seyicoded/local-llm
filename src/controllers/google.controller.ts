ce1d553760b626c900ab19b764d5b29a1bd0c347
seyicoded <opadonuseyi01@gmail.com>
2024-03-04 17:05:04 +0100

    processing data logically #2

--- src/controllers/google.controller.ts
+++ src/controllers/google.controller.ts
@@ -3,12 +3,15 @@ import { WrapperResponse } from "../helper/wrapResponse";
 import { google } from 'googleapis';
 import {GoogleAuth, OAuth2Client} from 'google-auth-library';
 import "dotenv/config";
+import axios from "axios";
 
 var striptags = require('striptags');
 const { convert } = require('html-to-text');
 var base64 = require('base-64');
 // const {GoogleAuth} = require('google-auth-library');
 // const {google} = require('googleapis');
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 export const getGoogleAuth = async (request: Request|any, response: Response)=>{
     
@@ -133,6 +136,95 @@ const getGoogleMailsSummary = async(request: Request|any, response: Response)=>{
     }, response);
 }
 
+type myMailsProps = {
+    title: string,
+    date: string,
+    threadId: string,
+    bodyMain: string,
+    bodyPart: string[],
+}
+const fetchRawSummaryAI = async (myMails: myMailsProps[])=>{
+    let myTranscriptData = "";
+    
+    myMails.map(({bodyMain, title, bodyPart})=>{
+        myTranscriptData += title;
+        if(bodyMain.length != 0){
+            myTranscriptData += bodyMain;
+            myTranscriptData += "\n";
+        }
+
+        bodyPart.map((partText)=>{
+            myTranscriptData += partText;
+            myTranscriptData += "\n";
+        })
+
+    });
+
+    console.log('before AIData');
+    // using ai to analyse
+    const AIData  = await axios({
+        url: 'https://api.openai.com/v1/chat/completions',
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_API_KEY}`
+        },
+        data: {
+            "model": (myTranscriptData.length < 4097) ? 'gpt-3.5-turbo' : "gpt-3.5-turbo-16k-0613",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": `generate data extrememly summarized in this pattern: “Give me a summary of last week” 
+                    I will give you a breakdown of your calendar
+                    
+                    Meetings
+                    You had 8 meetings 
+                    You accepted 5, declined 1, 2 with no response
+                    Last week Thursday you had “Meeting title” with Idris & Seyi
+                    You discussed (summary of meeting)
+                    On Mon 24th, you went to “Location in meeting” for “Meeting title”
+                    
+                    I can also check your email without invading your privacy
+                    Privacy & safety is important to us with end-to-end encryption and your summary is only visible to you
+                    You can set a password for future email summaries
+                    
+                    Gmail
+                    Here’s a summary of your email broken down into categories
+                    48 receipts, with a total of $XXX from Careem, Talabat & more
+                    Food: $XXX, Software: $XX, Insurance $X
+                    🧾 5 invoices from Github, Figma, Bing & more
+                    Paid 3, 1 invoice is due next week, 0 overdue
+                    🤑 12 bank transfers with a total of $XXX
+                    Outgoing: $XXX
+                    Incoming: $XXX
+                    ✈️ 2 travel itinerary
+                    Upcoming flight to Amsterdam next week Monday
+                    💼 5 new jobs listing from Linkedin, Indeed and +3 more
+                    ✉️ 2 emails sent are still unread
+                    29 documents were shared with you via G Drive, Notion +27
+                    Firstname Lastname shared “File title”
+                    🎨 117 Comments on files from Figma, G Doc / Slides & more
+                    Figma comments on “Project title” from “Firstname  Lastname” and 10 others
+                    Firstname Lastname: Change design title 
+                    359 Direct messages on Slack, Linkedin, Teams & more
+                    Linkedin: 120 direct messages 
+                    Slack: 56 messages on 35 channels
+                    Teams: 9 files were shared with you
+                    1,985 notifications from Facebook, Instagram, twitter & more
+                    217 vouchers worth $XXX from Asos, Gucci, Zara & more`
+                },
+                {
+                    "role": "user",
+                    "content": `here's the data ${((myTranscriptData.length > 16250)) ? myTranscriptData.substring(0, 16250) : myTranscriptData}`
+                },
+            ],
+            "temperature": 0.2
+        }
+    });
+
+    return AIData?.data;
+}
+
 const processString = (message: string)=>{
     // Regular expression to match URLs starting with http:// or https://
     var urlRegex = /(https?:\/\/[^\s]+)/g;
@@ -159,4 +251,4 @@ const replaceAll = (inputStr: string, oldSubstring: string, newSubstring: string
     // Use regular expression with global flag to replace all occurrences
     const regex = new RegExp(oldSubstring, 'g');
     return inputStr.replace(regex, newSubstring);
-  }
+}
